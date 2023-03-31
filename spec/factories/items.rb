bfa38f11f69e3023da6d033f716216cc4bb2c1d4
nakamura1984 <126934364+nakamura1984@users.noreply.github.com>
2023-03-31 17:01:07 +0900

    テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,15 +1,16 @@
 FactoryBot.define do
   factory :item do
-    name {'テスト'}
-    detail {'太郎'}
-    category_id {'山田'}
-    condition_id {'タロウ'}
-    shipping_cost_id {'ヤマダ'}
-    area_of_origin_id {"1980-1-1"}
-    estimated_sipping_date_id {'テスト'}
-    selling_price{"300"}
+    name { 'テスト' }
+    detail { 'テスト' }
+    shipping_cost_id { 2 }
+    category_id  { 2 }
+    condition_id  { 2 }
+    area_of_origin_id  { 2 }
+    estimated_sipping_date_id  { 2 }
+    selling_price  { 300 }
     after(:build) do |item|
-      item.images.attach(io: File.open('pubic/images/test_image.jpg'), filename: 'test_image.jpg', content_type: 'image/jpg')
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png', content_type: 'image/jpg')
     end
+    association :user
   end
 end
