9aa5461502a6e4a457a8fb4675bfc841bd8c0d81
nakamura1984 <126934364+nakamura1984@users.noreply.github.com>
2023-04-03 17:05:56 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
+  before_action :set_item, only: :edit
+  before_action :move_to_index, only: :edit
+
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -27,9 +30,28 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :category_id, :condition_id, :shipping_cost_id, :area_of_origin_id, :estimated_sipping_date_id, :selling_price, :name, :detail).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    if  current_user.id != @item.user_id
+      redirect_to action: :index
+    end
   end
 end
