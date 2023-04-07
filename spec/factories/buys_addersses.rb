aa3a7389d6ed5fe19daac84530806d39b1c9aaa5
nakamura1984 <126934364+nakamura1984@users.noreply.github.com>
2023-04-07 17:26:38 +0900

    テストコード作成

---
+++ spec/factories/buys_addersses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :buy_address do
+    address { '青山' }
+    municipality { '渋谷区' }
+    post_code  { "111-1111" }
+    telephone_number  { "09011111111" }
+    area_of_origin_id  { 2 }
+    association :user, :item
+  end
+end
