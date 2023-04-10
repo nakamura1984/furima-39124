59f91deafc1106285e8252b36b6cb6a81b27b43c
nakamura1984 <126934364+nakamura1984@users.noreply.github.com>
2023-04-10 17:59:35 +0900

    修正しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,7 +52,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    return unless current_user.id != @item.user_id
+    return unless current_user.id != @item.user_id || @item.buy.present?
     redirect_to action: :index
   end
 end
